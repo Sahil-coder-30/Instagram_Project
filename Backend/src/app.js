c49aa1f102aac65f539d32847d44f225d724d068
PRAVEEN <praveen.iyu123@gmail.com>
2026-04-07 17:00:44 +0530

    no change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,8 +3,6 @@ const CookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
 
-/** Middelware used.. */
-
 app.use(express.json());
 app.use(CookieParser());
 app.use(cors({
@@ -12,15 +10,9 @@ app.use(cors({
     origin: "https://web-dev-lyft.vercel.app"
 }))
 
-
-/**routes required here... */
-
 const authRouter = require("./routes/auth.routes");
 const postRouter = require("./routes/post.routes");
 const userRouter = require("./routes/user.routes");
-
-
-/**Routes user here ... */
 
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
